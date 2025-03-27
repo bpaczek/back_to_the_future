e4eb2e178756314b15cefeab415696c0e12a44dc
Bartosz Paczek <paczek.bartosz@gmail.com>
2025-03-27 20:08:16 +0100

    Modify explorer class

--- explorer.h
+++ explorer.h
@@ -2,31 +2,22 @@
 #define EXPLORER_H
 
 #include <filesystem>
-#include <string>
-#include <vector>
+#include <memory>
 #include "status.h"
-#include "IExplorer.h"
 
 namespace fs = std::filesystem;
 
-class Explorer : public IExplorer {
-    public: 
+class Explorer {
+public:
     Explorer();
     Explorer(std::string location);
-    
-    std::string GetCurrentLocationPath();
-    std::filesystem::directory_entry GetCurrentLocation();
-    void GetCurrentLocationContent();
-    void PrintCurrentLocationContent();
-    void MoveToLocation(std::filesystem::directory_entry location);
-    std::filesystem::directory_entry GetLocationItembyId(uint32_t id);
+    ~Explorer();
+
     Status SelectItemToArchive(std::filesystem::directory_entry* location = nullptr);
-    std::filesystem::directory_entry GetLocation();
-    private:
-    std::filesystem::directory_entry CurrentLocation;
-    std::vector<std::filesystem::directory_entry> LocationContent;
-    static void PrintHelp();
-    void SetCurrentLocation(std::filesystem::directory_entry location);        
+
+private:
+    class Impl; // Forward declaration of implementation class
+    std::unique_ptr<Impl> pImpl; // Pointer to implementation
 };
 
 #endif
